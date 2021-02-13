a5ae0ede1f19d3e9f933bda247c822154de0166c
ThuHtetDev <thuhtettun.dev@gmail.com>
2021-02-13 16:04:27 +0630

    test name

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,7 +19,9 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <style>
-        [v-cloak] { display:none; }
+        [v-cloak] { 
+            display:none;
+        }
     </style>
 </head>
 <body>
